af8444da3c1430aaded50233075c4993c2635463
orangeyf <orangeyf3@gmail.com>
2016-02-06 15:08:27 -0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-  		<link rel="stylesheet" href="css/bootstrap.css" >
+  		  <link rel="stylesheet" href="css/bootstrap.css" >
 		  <script src="js/bootstrap.js" > </script>
 		  <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
 		  <link href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,100italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
