bc0443154850758841b842f085e7bc703b9fde96
orangeyf <orangeyf3@gmail.com>
2016-02-06 17:08:18 -0800

    Added more of the body

--- profile.php
+++ profile.php
@@ -1,25 +1,37 @@
 <html>
   <head>
-  		  <link rel="stylesheet" href="css/bootstrap.css" >
-		  <script src="js/bootstrap.js" > </script>
-		  <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
-		  <link href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,100italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
-		  <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
-		  <link href='css/gallery.css' rel='stylesheet' type='text/css' media='screen'>
-		  <link href='css/style.css' rel='stylesheet' type='text/css' media='screen'>
-		  <title>ESHS WebCalc</title>
-		</head>
+  	<link rel="stylesheet" href="css/bootstrap.css" >
+	<script src="js/bootstrap.js" > </script>
+	<link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,100italic,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+	<link href='css/gallery.css' rel='stylesheet' type='text/css' media='screen'>
+	<link href='css/style.css' rel='stylesheet' type='text/css' media='screen'>
+	<title>ESHS WebCalc</title>
+</head>
 	<body>
-	  <div class="nav">
-	    <a href="#">
-				<div class="nav-elem navbar-left" style="">ESHS WebCalculator</div>
-	    </a>
-	    <a href="stats.php"?
-			  <div class="nav-elem">Your Stats</div>
-			</a>
-			<a href="prof.php"?
-			  <div class="nav-elem">Your Profile</div>
-			</a>
+		<div class="nav">
+	<a href="#">
+		<div class="nav-elem navbar-left" style="">ESHS WebCalculator</div>
+	</a>
+	<a href="stats.php"?
+		<div class="nav-elem">Your Stats</div>
+	</a>
+	<a href="prof.php"?
+		<div class="nav-elem">Your Profile</div>
+	</a>
+	<div class="body">
+		<div class="section">
+			<h1>Welcome to your profile!</h1
+			<p>Here, you can view your stats, your profile, and your feed.
+		</div>
+		<br>
+		<div class="section">
+			<h1>Your feed</h1>
+			<p>Content here</p>
+		</div>
+			
+	</div>
 	</body>
 </html>
 <!--Why won't it let me make my tabs 2?-->
